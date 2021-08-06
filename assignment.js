0b135cb4e4ddafedc1cd2ea7a62368b0037ed43a
jdashvvenus <88146896+jdashvvenus@users.noreply.github.com>
2021-08-06 17:30:19 +0800

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,30 @@
+/*
+let country = 'Philippines';
+let continent = 'Asia';
+let population = '100000000';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let language = 'Filipino';
+const finlandPopulation = 6000000;
+const avgPopulation = 33000000;
+
+console.log(population / 2);
+console.log(population > finlandPopulation);
+console.log(population < avgPopulation);
+const description = country + ' is in ' + continent + ' and its ' + population + ' people speak ' + language + '.';
+console.log(description);
+
+let johnHeight1 = "1.95"
+let johnWeight1 = "92"
+let markHeight1 = "1.69"
+let markWeight1 = "78"
+
+const BMIMark = markWeight1 / markHeight1 ** 2;
+const BMIJohn = johnWeight1 / johnHeight1 ** 2;
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn
+console.log(markHigherBMI)
+*/
---
+++ script.js
@@ -0,0 +1,79 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is FUN!');
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let isIsland = true;
+console.log(typeof isIsland);
+let language;
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+let age = 30;
+age 31;
+const birthYear = 1991;
+
+// basic operators
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x + x -1 = 100
+x--; // x + x -1 = 99
+console.log(x);
+
+//comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+
+let z, y;
+z = y = 25 - 10 - 5;
+console.log(z, y);
+
+
+//strings & template literals
+const firstName = 'Juherm';
+const job = 'engineer';
+const birthYear = 1997;
+const year = 2020;
+
+const juherm = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(juherm);
+
+const juhermNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(juhermNew);
+
+console.log(`String with \n\
+multiple \n\
+lines`);
+
+console.log(`String with
+multiple
+lines`);
+*/
