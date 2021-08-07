2861aec2496871dc291423b748a3268448fe4edd
jdashvvenus <88146896+jdashvvenus@users.noreply.github.com>
2021-08-07 21:27:27 +0800

    fundamentals 2 final

---
+++ FUNDAMENTALS 2/activities.js
@@ -0,0 +1,42 @@
+//ARRAYS
+/*
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+
+
+//OBJECT METHOD
+const jacob = {
+    fullName: 'Jacob Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.BMI = this.mass / this.height ** 2
+        return this.BMI
+    }
+};
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.BMI = this.mass / this.height ** 2
+        return this.BMI
+    }
+};
+
+jacob.calcBMI();
+john.calcBMI();
+
+console.log(jacob.BMI, john.BMI);
+
+if (jacob.BMI > john.BMI) {
+    console.log(`${jacob.fullName}'s BMI ${jacob.BMI} is higher tham ${john.fullName}'s BMI ${john.BMI}`)
+} else if (john.BMI > jacob.BMI) {
+    console.log(`${john.fullName}'s BMI ${john.BMI} is higher tham ${jacob.fullName}'s BMI ${jacob.BMI}`)
+};
+*/
